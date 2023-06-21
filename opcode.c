db00ebb2bf2c4ec1e2b14105485eb597b573dd03
Noha Fathy <nohazaid97@gmail.com>
2023-06-20 17:02:24 -0700

    Noha: Opode file

---
+++ opcode.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * ops_acq - acquires the corresponding function of the opcode
+ *
+ * Description:
+ * @opCode: opcode from bytecode
+ *
+ * Return: NULL if function is invalid
+*/
+
+void (*ops_acq(char *opCode))(stack_t **stack, unsigned int lineNumb)
+{
+	int parv = 0;
+
+	instruct_t op_list[] = {
+		{"push", push},
+		{"pall", pall},
+		{NULL, NULL},
+	};
+
+	while (op_list[parv].opCode)
+	{
+		if (strcmp(opCode, op_list[i].opCode) == 0)
+			return (op_list[parv].func);
+		parv++;
+	}
+	return (NULL);
+}
