586a512af40769e1b86e770b6ca43afcd46ff974
Joshua <kenduajayi@gmail.com>
2023-06-22 06:56:58 -0700

    Parv: added file

---
+++ opcodeNop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - opcode
+ */
+
+void nop(__attribute__((unused))stack_t **stack, __attribute__((unused))unsigned int line_number)
+{
+	/**
+	 * the opcode NOP is an equal in c
+	 * therefore print 0
+	 */
+	;
+}
